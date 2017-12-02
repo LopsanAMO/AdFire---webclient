e6de3fd59979782eb07d0b660761d1619109c79a
Rolando Escalona <rolando.escalona@gmail.com>
2017-12-02 15:59:14 -0600

    form, alpha version

---
+++ adfire/src/app/form/form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.styl']
+})
+export class FormComponent implements OnInit {
+
+
+  ngOnInit() {
+  
+  }//ngOnInit
+
+
+}
